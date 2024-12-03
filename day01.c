feb7c040cf07c9b7ce3ab40b032f9a3a6514583b
Eero Lehtinen <eero@eerolehtinen.fi>
2024-12-04 01:28:29 +0200

    Day 1

--- day01.c
+++ day01.c
@@ -1,3 +1,5 @@
+#include <errno.h>
+#include <stdio.h>
 #include <string.h>
 #include <stddef.h>
 #include <stdbool.h>
@@ -5,99 +7,190 @@
 #include <stdlib.h>
 
 struct Match {
-	int start;
-	int end;
-	char* value;
+	const char* start;
+	const char* value;
 };
 
-char* substring(const char* src, size_t start, size_t end) {
-    if (src == NULL || start > end || start < 0) {
-        return NULL;
-    }
-    size_t length = end - start + 1;
-    char* dest = malloc(length + 1);
-    if (dest == NULL) {
-        return NULL;
-    }
-    strncpy(dest, src + start, length);
-    dest[length] = '\0';
 
-    return dest;
+char* substring(const char* start, const char* end) {
+	assert(start != NULL || end != NULL || start < end);
+
+	size_t length = end - start;
+
+	char* dest = malloc(length + 1);
+	assert(dest != NULL);
+
+	strncpy(dest, start, length);
+	dest[length] = '\0';
+
+	return dest;
 }
 
-bool match(char* pattern, char* input, struct Match* matches, size_t matches_count) {
-	for (int i = 0; i < matches_count; i++) {
-		matches[i].start = -1;
-		matches[i].end = -1;
+const char ESCAPE = '\\';
+
+bool match_inner(const char* pattern, const char* input, struct Match* matches, size_t matches_count, size_t im) {
+	if (*pattern == '\0') {
+		return true;
 	}
-
-	size_t ip = 0;
-	size_t ii = 0;
-	size_t im = 0;
-
-	const char ESCAPE = '\\';
-
-	while (true) {
-		struct Match* mat = &matches[im];
-		char pat = pattern[ip];
-		char inp = input[ii];
-		if (pat == '\0') {
-			break;
-		}
-		if (inp == '\0') {
-			if (pat != '\0') {
-				return false;
-			}
-			break;
-		}
-		if (pat == ESCAPE) {
-			ip++;
-			if (pat == 'd' && inp >= '0' && inp <= '9') {
-
+	const char* pat = pattern;
+	if (*pat == ESCAPE) {
+		++pat;
+		if (*pat == 'd') {
+			if (*input >= '0' && *input <= '9') {
+				// matched
 			} else {
 				return false;
 			}
-		}
-		else if (pat == '(') {
-			if (mat->start != -1 || mat->end != -1) {
-				return false;
-			}
-			mat->start = ii;
-			ip++;
-		}
-		else if (pat == ')') {
-			if (mat->start == -1 || mat->end != -1) {
-				return false;
-			}
-			mat->end = ii;
-			ip++;
-		}
-		else if (pat != inp) {
-			return false;
-			ip++;
-			ii++;
-		}
-		else {
+		} else {
 			return false;
 		}
+		++input;
+	} else if (*pat == '(') {
+		struct Match* mat = &matches[im];
+		if (mat->start != NULL) {
+			return false;
+		}
+		mat->start = input;
+	} else if (*pat == ')') {
+		if (im >= matches_count) {
+			return false;
+		}
+		struct Match* mat = &matches[im];
+		if (mat->start == NULL) {
+			return false;
+		}
+		mat->value = substring(mat->start, input);
+		im++;
+	} else if (*pat == *input) {
+		++input;
+	} else {
+		return false;
 	}
 
-	return true;
+	if (*(pat + 1) == '+') {
+		// First try to greedily match more
+		bool res = match_inner(pattern, input, matches, matches_count, im);
+		if (!res) {
+			// If didn't work, stop greeding
+			res  = match_inner(pat + 2, input, matches, matches_count, im);
+		}
+		return res;
+
+	} else {
+		return match_inner(pat + 1, input, matches, matches_count, im);
+	}
+}
+
+
+
+bool match(const char* pattern, const char* input, struct Match* matches, size_t matches_count) {
+	for (int i = 0; i < matches_count; i++) {
+		matches[i].start = NULL;
+		matches[i].value = NULL;
+	}
+
+	return match_inner(pattern, input, matches, matches_count, 0);
+}
+
+void match_assert(struct Match* m1, const char** m2, size_t count) {
+	for (size_t i = 0; i < count; i++) {
+		if (m1[i].value == NULL || m2[i] == NULL) {
+			assert(m1[i].value == NULL);
+		} else {
+			assert(strcmp(m1[i].value, m2[i]) == 0);
+		}
+	}
+}
+
+void test() {
+	const size_t matches_count = 3;
+	struct Match matches[matches_count];
+
+	assert(match("asd", "asd", matches, matches_count));
+	assert(!match("bcd", "asd", matches, matches_count));
+	assert(match("\\d\\d\\d", "123", matches, matches_count));
+	assert(!match("\\dx", "12", matches, matches_count));
+	
+	assert(match("(\\d) (\\d)", "1 2", matches, matches_count));
+	match_assert(matches, (const char*[3]){ "1", "2", NULL },  3);
+
+	assert(match("(\\d+) +(\\d+)", "12	 34\n", matches, matches_count));
+	match_assert(matches, (const char*[3]){ "12", "34", NULL }, 3);
+}
+
+FILE* open_file(const char *filename) {
+	FILE *file = NULL;
+
+	file = fopen(filename, "r");
+	if (file == NULL) {
+		fprintf(stderr, "Error opening file %s: %s\n", filename, strerror(errno));
+		return NULL;
+	}
+
+	return file;
+}
+
+
+const size_t MAX_LINE_LENGTH = 1024;
+
+
+int compare(const void* a, const void* b) {
+   return (*(int*)a - *(int*)b);
 }
 
 int main() {
-	const size_t MATCHES_COUNT = 0;
-	struct Match matches[MATCHES_COUNT];
+	FILE* file = open_file("input");
+	assert(file);
 
-	assert(match("asd", "asd", matches, MATCHES_COUNT));
-	assert(!match("bcd", "asd", matches, MATCHES_COUNT));
-	assert(match("\\d\\d\\d", "123", matches, MATCHES_COUNT));
-	assert(!match("\\dx", "12", matches, MATCHES_COUNT));
+	char line[MAX_LINE_LENGTH];
+	const size_t matches_count = 2;
+	struct Match matches[matches_count];
 
-	assert(match("(\\d) (\\d)", "1 2", matches, MATCHES_COUNT));
-	assert(matches[0].start == 0);
-	assert(matches[0].end == 1);
-	assert(matches[1].start == 2);
-	assert(matches[1].end == 3);
-	assert(matches[2].start == -1);
+	const size_t nums_max = 10000;
+	int nums1[nums_max];
+	int nums2[nums_max];
+
+	size_t nums_size = 0;
+	while (fgets(line, sizeof(line), file) != NULL) {
+		match("(\\d+) +(\\d+)", line, matches, matches_count);
+		if (matches[0].value == NULL) {
+			break;
+		}
+		nums1[nums_size] = atoi(matches[0].value);
+		nums2[nums_size] = atoi(matches[1].value);
+		nums_size++;
+	}
+
+	qsort(nums1, nums_size, sizeof(int), compare);
+	qsort(nums2, nums_size, sizeof(int), compare);
+
+	int result = 0;
+	int similarity = 0;
+	size_t i = 0;
+	size_t j = 0;
+	while (i < nums_size && j < nums_size) {
+		if (nums1[i] == nums2[j]) {
+			similarity++;
+			j++;
+		} else	{
+			result += similarity * nums1[i];
+			if (nums1[i] < nums2[j]) {
+				int n = nums1[i];
+				i++;
+				while (i < nums_size && nums1[i] == n) {
+					result += similarity * n;
+					i++;
+				}
+			}
+			while (nums1[i] > nums2[j]) {
+				j++;
+			}
+			similarity = 0;
+		}
+	}
+
+	printf("%d", result);
+
+	fclose(file);
 }
+
